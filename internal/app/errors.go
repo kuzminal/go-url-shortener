8a8a8adc7d51c327bcd1e097ca4f93b5ee549fd5
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2023-11-30 16:43:01 +0300

    Инкремент 24: - поправил замечания автотестов

--- internal/app/errors.go
+++ internal/app/errors.go
@@ -2,6 +2,7 @@ package app
 
 import "errors"
 
+// Типы пользовательских ошибок
 var (
 	ErrAuth      = errors.New("auth unprocessed")                 // ErrAuth ошибка авторизации
 	ErrParseURL  = errors.New("cannot parse given string as URL") // ErrParseURL ошибка парсинга строки в URL
